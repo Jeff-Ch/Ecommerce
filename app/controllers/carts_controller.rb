f4b28945f2e9ce1bc61d531b475cfc04d3c38de9
Jeff-Ch <jefchien@gmail.com>
2016-01-07 21:09:07 -0800

    Added remove and adding items already in cart functions

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,13 +8,29 @@ class CartsController < ApplicationController
   end
 
   def update
+  	@current_product = Cart.where(:user_id => session[:user_id]).where(:product_id => params[:id]).first
+  	if @current_product
+  		@quantity = @current_product.quantity + params[:quantity].to_i
+  		Cart.update(@current_product.id, :quantity => @quantity)
+  	else
   	Cart.create(:user_id => session[:user_id], :product_id => params[:id], :quantity => params[:quantity])
+  end
   end
 
   def delete
   end
 
   def remove
-  	@params = params
+  	@current_product = Cart.where(:user_id => session[:user_id]).where(:product_id => params[:id]).first
+  	@quantity = @current_product.quantity - params[:quantity].to_i
+  	puts "GIIIII"
+  	puts @quantity
+  	puts "DJSJDSJDSJ"
+  	if @quantity == 0
+  		Cart.destroy(@current_product.id)
+  	else
+  		Cart.update(@current_product.id, :quantity => @quantity)
+  	end
+  	redirect_to "/carts/index"
   end
 end
