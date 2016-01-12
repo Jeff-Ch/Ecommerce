0d55ed53c4df7eb82bab573d047dc8ebd063d885
Jeff-Ch <jefchien@gmail.com>
2016-01-12 12:23:55 -0800

    Added recent products to home

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,11 @@ class DashboardController < ApplicationController
 
   def index
   	@user = Username.where(:user_id => session[:user_id]).first
+  	@recent = Cart.order('order_id DESC')
+  	@recent_items = []
+  	@recent.each do |cart_item|
+  		@recent_items.push(Product.find(cart_item.product_id))
+  	end
+  	@recent_items.uniq!
   end
 end
